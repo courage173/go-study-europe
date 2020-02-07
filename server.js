43dcbedfa9cef9591e370e93edf08b5f4ca20c39
courage173 <courageosemwengie@gmail.com>
2020-02-07 19:31:23 +0100

    refactored

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require ("express")
+const nodemailer = require("nodemailer")
 
 const app = express()
 app.use(express.json())
@@ -6,7 +7,27 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'static')));
 
+const transporter =nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "courageosemwengie@gmail.com",
+        pass: 'faithbosky'
+    }
+})
+const mailOptions = {
+    from: 'courageosemwengie@gmail.com', // sender address
+    to: 'osamudiamenogbiede55@gmail.com', // list of receivers
+    subject: 'Subject of your email', // Subject line
+  }
 
+  transporter.sendMail(mailOptions, function(err,info){
+      if(err){
+          console.log(err)
+      }
+      else{
+          console.log(info)
+      }
+  })
 
 const port = (process.env.PORT || '8000');
 
